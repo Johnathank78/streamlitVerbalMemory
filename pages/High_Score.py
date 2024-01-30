22a397b946a3f382e596386205545edcb8eae261
Johnathank78 <107622965+Johnathank78@users.noreply.github.com>
2024-01-30 20:09:00 +0100

    DB upd

--- pages/High_Score.py
+++ pages/High_Score.py
@@ -6,10 +6,27 @@ from sqlalchemy import create_engine, text
 engine = create_engine("sqlite:///highScores.db")
 conn = engine.connect()
 
-highScore = conn.execute("SELECT * FROM highScores").fetchone()
+# Insert some data with conn.session.
+
+conn.execute(text('''
+    CREATE TABLE IF NOT EXISTS players (
+        uuid TEXT PRIMARY KEY, 
+        Date TEXT, 
+        Name TEXT, 
+        Score INT
+    );
+'''))
+
 conn.commit()
+
+
+query = conn.execute(text("SELECT * FROM players"))
+conn.commit()
+
+highScores = pandas.DataFrame(query.all(), columns=query.keys())
+highScores = highScores.drop('uuid', axis=1)
 
 with st.container():
     x1, x2, x3 = st.columns([1,2,1])
     with x2:
-        st.dataframe(highScore)
+        st.dataframe(highScores)
